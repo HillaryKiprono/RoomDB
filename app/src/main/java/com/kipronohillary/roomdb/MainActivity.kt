abe2196468f227824cb76116888c2acb00b7966c
Hillary Kiprono <kipronohillary240@gmail.com>
2022-11-09 19:07:15 +0300

    insert&&getData

--- app/src/main/java/com/kipronohillary/roomdb/MainActivity.kt
+++ app/src/main/java/com/kipronohillary/roomdb/MainActivity.kt
@@ -2,10 +2,73 @@ package com.kipronohillary.roomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.kipronohillary.roomdb.databinding.ActivityMainBinding
+import com.kipronohillary.roomdb.models.AppDatabase
+import com.kipronohillary.roomdb.models.Student
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appDb:AppDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnWriteData.setOnClickListener {
+            writeData()
+        }
+        binding.btnReadData.setOnClickListener {
+            readData()
+        }
+
     }
+
+
+
+    private fun writeData() {
+        val firstname=binding.etFirstName.text.toString()
+        val lastname=binding.etLastName.text.toString()
+        val rollNo=binding.etRollNo.text.toString()
+        if (firstname.isNotEmpty() && lastname.isNotEmpty() && rollNo.isNotEmpty()){
+            val student=Student(
+                    null,firstname,lastname,rollNo.toInt()
+            )
+            GlobalScope.launch (Dispatchers.IO){
+                appDb.studentDao().insert(student)
+            }
+            binding.etFirstName.text.clear()
+            binding.etLastName.text.clear()
+            binding.etRollNo.text.clear()
+            Toast.makeText(this@MainActivity, "Successfully Inserted", Toast.LENGTH_SHORT).show()
+
+
+        }
+        else
+        {
+            Toast.makeText(this@MainActivity, "Please enter the data", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private  suspend fun displayData(student: Student){
+        withContext(Dispatchers.Main){
+            binding.tvFirstName.text=student.firstName
+            binding.tvLastName.text=student.lastName
+            binding.tvRollNo.text=student.rollNo.toString()
+        }
+    }
+    private fun readData() {
+        val rollNo=binding.etRollNoRead.text.toString()
+        if (rollNo.isNotEmpty()){
+            lateinit var student: Student
+
+            GlobalScope.launch {
+                student=appDb.studentDao().findByRoll(rollNo.toInt())
+                displayData(student)
+            }
+        }
+    }
+
 }
