fd8aba660f3ed02a0a3e86e7d1e7a566537a45d0
Hillary Kiprono <kipronohillary240@gmail.com>
2022-11-09 18:44:18 +0300

    layout

--- app/src/main/java/com/kipronohillary/roomdb/models/AppDatabase.kt
+++ app/src/main/java/com/kipronohillary/roomdb/models/AppDatabase.kt
@@ -3,9 +3,7 @@ package com.kipronohillary.roomdb.models
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
-import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
-import java.text.Normalizer.Form
 
 @Database(entities = [Student::class], version = 1)
  abstract class AppDatabase :RoomDatabase(){
